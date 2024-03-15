a7a3fe4798f962dad92a28a3bac987466fbe0882
tracer-ics2017 <tracer@njuics.org>
2024-03-15 19:56:34 +0800

    >  run 2110508 ybx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  19:56:34 up 16:51,  3 users,  load average: 0.09, 0.12, 0.16 c8271103a6f74f4eeb2820ab892e7ba44962e534

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -89,13 +89,13 @@ static int cmd_help(char *args) {
 //单步调试
 static int cmd_si(char *args) {
   uint64_t N = 0;
-  if(args == NULL) {
-    N = 1;
+  if(args == NULL) { //没有参数时
+    N = 1; //默认为1
   }
   else {
-    int temp = sscanf(args, "%llu", &N);
-    if(temp <= 0) {
-      printf("args error in cmd_si\n");
+    int temp = sscanf(args, "%llu", &N); //读取参数
+    if(temp <= 0) { //解析失败
+      printf("ERROR:args error in cmd_si\n");
       return 0;
     }
   }
@@ -105,23 +105,21 @@ static int cmd_si(char *args) {
 //打印程序状态
 static int cmd_info(char *args) { 
   char s;
-  if(args == NULL) {
-    printf("args error in cmd_info (miss args)\n");
+  if(args == NULL) { //没有参数时
+    printf("ERROR:args error in cmd_info (miss args)\n");
     return 0;
   }
   int temp = sscanf(args, "%c", &s);
-  if(temp <= 0) {
+  if(temp <= 0) { //解析失败
     //解析失败
     printf("args error in cmd_info\n");
     return 0;
   }
-  if(s == 'w') {
-    //打印监视点信息
+  if(s == 'w') { //打印监视点信息
     print_wp();;
     return 0;
   }
-  if(s == 'r') {
-    //打印寄存器
+  if(s == 'r') { //打印寄存器信息
     for(int i = 0; i < 8; i++) {
       printf("%s  0x%x\n", regsl[i], reg_l(i));
     }
@@ -137,21 +135,19 @@ static int cmd_info(char *args) {
     }
     return 0;
   }
-  //如果产生错误
-  printf("args error in cmd_info\n");
+  printf("ERROR:args error in cmd_info\n"); //如果产生错误报错
   return 0;
 }
 //内存扫描
 static int cmd_x(char *args) {
-  int nLen = 0;
-  vaddr_t addr;
-  int temp = sscanf(args, "%d 0x%x", &nLen, &addr);  //sscanf()函数用于从字符串中读进与指定格式相符的数据
-  if(temp <= 0) {
-    //解析失败
-    printf("args error in cmd_si\n");
+  int nLen = 0; //长度
+  vaddr_t addr; //起始地址
+  int temp = sscanf(args, "%d 0x%x", &nLen, &addr);  //从字符串中读进与指定格式相符的数据
+  if(temp <= 0) { //解析失败
+    printf("ERROR:args error in cmd_si\n");
     return 0;
   } 
-  printf("Memory:"); 
+  printf("Memory:"); //打印内存
   for(int i = 0; i < nLen; i++) {  
     if(i % 4 == 0) { 
       printf("\n0x%x:  0x%02x", addr + i, vaddr_read(addr + i, 1));
@@ -166,35 +162,34 @@ static int cmd_x(char *args) {
 //计算表达式的值
 static int cmd_p(char *args) {
   bool is_success;
-  int temp = expr(args, &is_success);
-  if(is_success == false) {
-    printf("error in expr()\n");
+  int temp = expr(args, &is_success); //计算表达式
+  if(is_success == false) { //表达式错误
+    printf("ERROR:error in expr()\n");
   }
   else {
-    printf("the value of expr is:%d\n", temp);
+    printf("the value of expr is:%d\n", temp); //打印表达式的值
   }
   return 0;
 }
 //添加监视点
 static int cmd_w(char *args) { 
-  new_wp(args);
+  new_wp(args); //分配监视点
   return 0;
 }
 //删除监视点
 static int cmd_d(char* args) {
   int num = 0;
-  int nRet = sscanf(args, "%d", &num);
-  if(nRet <= 0) {
-    //解析失败
+  int nRet = sscanf(args, "%d", &num); //解析参数
+  if(nRet <= 0) { //解析失败
     printf("args error in cmd_si\n");
     return 0;
   }
   int r = free_wp(num); //删除监视点
-  if(r == false) {
-    printf("error: no watchpoint %d\n", num);
+  if(r == false) {//删除监视点失败
+    printf("ERROR: no watchpoint %d\n", num);
   }
   else {
-    printf("Success delete watchpoint %d\n", num);
+    printf("SUCCESS:Success delete watchpoint %d\n", num);
   }
   return 0;
 }
