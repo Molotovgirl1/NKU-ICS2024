bdf184f64cf7422e29235101056d96e45d7c0579
ybx <2110508@mail.nankai.edu.cn>
2024-03-16 09:31:38 +0800

    add explain

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -4,18 +4,18 @@
 #include "common.h"
 
 typedef struct watchpoint {
-  int NO; 
-  struct watchpoint *next;
+  int NO;  //序号
+  struct watchpoint *next; //指向下一个监视点的指针
 
   /* TODO: Add more members if necessary */
-  int old;  //储存表达式值
-  char e[32];  //表达式
+  int old;  //储存表达式的旧值
+  char e[32];  //储存表达式
   int hitNum;  //记录触发次数
 
 } WP;
-bool new_wp(char *arg); //新建监视点函数
-bool free_wp(int num);  //删除监视点函数
-void print_wp();        //打印监视点函数
-bool watch_wp();        //监视点值变化检测函数
+bool new_wp(char *arg); //新建监视点
+bool free_wp(int num);  //删除监视点
+void print_wp();        //打印监视点
+bool watch_wp();        //监视点值变化检测
 
 #endif
