9d3dcaf4e602c28127c8bae73dfdb67e2ad3e32b
tracer-ics2017 <tracer@njuics.org>
2024-03-31 22:59:00 +0800

    >  run 2110508 ybx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:59:00 up  1:53,  3 users,  load average: 0.40, 0.60, 0.57 fe38175f35ba039c565db01f3d79ad9a19551c9

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -142,52 +142,43 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-  rtl_subi(&cpu.esp, &cpu.esp, 4); 
-  rtl_sm(&cpu.esp, 4, src1); 
+  TODO();
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  rtl_lm(dest, &cpu.esp, 4);
-  rtl_addi(&cpu.esp, &cpu.esp, 4);
+  TODO();
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
-  rtl_sltui(dest, src1, 1);
+  TODO();
 }
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
   // dest <- (src1 == imm ? 1 : 0)
-  rtl_xori(dest, src1, imm);
-  rtl_eq0(dest, dest);
+  TODO();
 }
 
 static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 != 0 ? 1 : 0)
-  rtl_eq0(dest, src1);
-  rtl_eq0(dest, dest);
+  TODO();
 }
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  rtl_shri(dest, src1, width*8-1);
-  rtl_andi(dest, dest, 0x1);
+  TODO();
 }
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-   rtl_andi(&t0, result, (0xffffffffu >> (4-width)*8));
-  rtl_eq0(&t0, &t0);
-  rtl_set_ZF(&t0);
+  TODO();
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  assert(result != &t0);
-  rtl_msb(&t0, result, width);
-  rtl_set_SF(&t0);
+  TODO();
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
