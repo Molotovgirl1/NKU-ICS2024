24fa77396a13252d24f2d3d7b7676e101732b4b7
tracer-ics2017 <tracer@njuics.org>
2024-04-10 10:00:08 +0800

    >  run 2110508 ybx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:00:08 up 10:07,  3 users,  load average: 0.56, 0.33, 0.37 b0ac14404522a3689cad187327ebbd66df8a5d9f

--- nemu/include-wrong/monitor/watchpoint.h
+++
@@ -1,21 +0,0 @@
-#ifndef __WATCHPOINT_H__
-#define __WATCHPOINT_H__
-
-#include "common.h"
-
-typedef struct watchpoint {
-  int NO;
-  struct watchpoint *next;
-
-  /* TODO: Add more members if necessary */
- char str[32]; //表达式
- int value; //旧值
- int hitnum; //记录触发次数
-} WP;
-
-WP* new_wp(); //新建监视点
-bool free_wp(int NO); //删除监视点
-bool check_point(); 
-void show_point();
-
-#endif
