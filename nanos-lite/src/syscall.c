61d9d95f24a3401ea591d59ba819871e3ecaf046
ybx <2110508@mail.nankai.edu.cn>
2024-05-07 15:04:41 +0800

    add sys_write

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,25 +10,25 @@ void sys_exit(int a){
   _halt(a);
 }
 
-//int sys_write(int fd, void *buf, size_t len) {
-//	if(fd == 1 || fd == 2){
-//		char c;
-//    // Log("buffer:%s", (char*)buf);
-//		for(int i = 0; i < len; i++) {
-//			memcpy(&c ,buf + i, 1);
-//			_putc(c);
-//		}
-//		return len;
-//	}
-//  // else{
-//  //   panic("Unhandled fd=%d in sys_write()",fd);
-//  // }
+int sys_write(int fd, void *buf, size_t len) {
+	if(fd == 1 || fd == 2){
+		char c;
+    // Log("buffer:%s", (char*)buf);
+		for(int i = 0; i < len; i++) {
+			memcpy(&c ,buf + i, 1);
+			_putc(c);
+		}
+		return len;
+	}
+   else{
+     panic("Unhandled fd=%d in sys_write()",fd);
+   }
 //  if(fd >= 3) {
 //    return fs_write(fd, buf, len);
 //  }
 //  Log("fd <= 0");
-//	return -1;			
-//}
+	return -1;			
+}
 //
 //int sys_open(const char *pathname){
 //    return fs_open(pathname, 0, 0);
@@ -64,9 +64,9 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_exit: 
       sys_exit(a[1]);
       break;
-//    case SYS_write:
-//      SYSCALL_ARG1(r) = sys_write(a[1], (void*)a[2], a[3]);
-//      break;
+    case SYS_write:
+      SYSCALL_ARG1(r) = sys_write(a[1], (void*)a[2], a[3]);
+      break;
 //    case SYS_brk:
 //      SYSCALL_ARG1(r) = sys_brk(a[1]);
 //      break;
