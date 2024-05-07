65e06f5b58c56c78b891b074a47b5620d9818462
tracer-ics2017 <tracer@njuics.org>
2024-05-07 17:37:28 +0800

    >  run 2110508 ybx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:37:28 up 21:00,  3 users,  load average: 2.20, 2.55, 2.54 14f130a646797a0f649608dc7e59ae62edcde9ec

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,36 +13,39 @@ void sys_exit(int a){
 int sys_write(int fd, void *buf, size_t len) {
 	if(fd == 1 || fd == 2){
 		char c;
-		Log("buffer:%s",(char*)buf);
+//		Log("buffer:%s",(char*)buf);
 		for(int i = 0; i < len; i++) {
 			memcpy(&c ,buf + i, 1);
 			_putc(c);
 		}
 		return len;
 	}
+  	if(fd>=3){
+  		return fs_write(fd,buf,len);
+  	}
   	Log("fd <=0");
 	return -1;			
 }
-//
-//int sys_open(const char *pathname){
-//    return fs_open(pathname, 0, 0);
-//}
-//
-//int sys_read(int fd, void *buf,size_t len){
-//    return fs_read(fd, buf, len);
-//}
-//
-//int sys_lseek(int fd, off_t offset, int whence) {
-//    return fs_lseek(fd, offset, whence);
-//}
-//
+
+int sys_open(const char *pathname){
+    return fs_open(pathname, 0, 0);
+}
+
+int sys_read(int fd, void *buf,size_t len){
+    return fs_read(fd, buf, len);
+}
+
+int sys_lseek(int fd, off_t offset, int whence) {
+    return fs_lseek(fd, offset, whence);
+}
+
 int sys_brk(int addr) {
   return 0;
 }
-//
-//int sys_close(int fd){
-//    return fs_close(fd);
-//}
+
+int sys_close(int fd){
+    return fs_close(fd);
+}
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
@@ -64,18 +67,18 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_brk:
       SYSCALL_ARG1(r) = sys_brk(a[1]);
       break;
-//    case SYS_read:
-//      SYSCALL_ARG1(r) = sys_read(a[1],(void*)a[2],a[3]);
-//      break;
-//    case SYS_open:
-//      SYSCALL_ARG1(r) = sys_open((char*) a[1]);
-//      break;
-//    case SYS_close:
-//      SYSCALL_ARG1(r) = sys_close(a[1]);
-//      break;
-//    case SYS_lseek:
-//      SYSCALL_ARG1(r)=sys_lseek(a[1],a[2],a[3]);
-//      break;
+    case SYS_read:
+      SYSCALL_ARG1(r) = sys_read(a[1],(void*)a[2],a[3]);
+      break;
+    case SYS_open:
+      SYSCALL_ARG1(r) = sys_open((char*) a[1]);
+      break;
+    case SYS_close:
+      SYSCALL_ARG1(r) = sys_close(a[1]);
+      break;
+    case SYS_lseek:
+      SYSCALL_ARG1(r)=sys_lseek(a[1],a[2],a[3]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   return NULL;
