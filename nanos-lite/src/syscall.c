4c812690e4e39125b4c4c56fcdd3d988fb9f171c
tracer-ics2017 <tracer@njuics.org>
2024-05-07 15:33:00 +0800

    >  run 2110508 ybx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:33:00 up 18:55,  3 users,  load average: 0.35, 0.35, 0.27 d4692fd0f317e4cb35be35aa32b130b4c16e9454

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,20 +13,13 @@ void sys_exit(int a){
 int sys_write(int fd, void *buf, size_t len) {
 	if(fd == 1 || fd == 2){
 		char c;
-    // Log("buffer:%s", (char*)buf);
 		for(int i = 0; i < len; i++) {
 			memcpy(&c ,buf + i, 1);
 			_putc(c);
 		}
 		return len;
 	}
-   else{
-     panic("Unhandled fd=%d in sys_write()",fd);
-   }
-//  if(fd >= 3) {
-//    return fs_write(fd, buf, len);
-//  }
-//  Log("fd <= 0");
+  	Log("fd <=0");
 	return -1;			
 }
 //
