b4b7ce116340ba79bb996007fca740a81e1097c1
ybx <2110508@mail.nankai.edu.cn>
2024-05-07 16:20:16 +0800

    add file system

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,14 +1,21 @@
 #include "common.h"
+#include "fs.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
-void ramdisk_read(void *, uint32_t, uint32_t);
-size_t get_ramdisk_size();
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+
+#define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
+
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  //TODO();
-  size_t len = get_ramdisk_size();
-  ramdisk_read(DEFAULT_ENTRY, 0, len);
+  // TODO();
+  // ramdisk_read(DEFAULT_ENTRY, 0, RAMDISK_SIZE);
+  int fd = fs_open(filename, 0, 0);
+  Log("filename=%s,fd=%d",filename,fd);
+  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
-
 }
