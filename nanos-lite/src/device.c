e15cb7549c5cb4e21d54486fb9a077a5b0ff800b
tracer-ics2017 <tracer@njuics.org>
2024-05-08 10:36:19 +0800

    >  run 2110508 ybx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:36:19 up 21:35,  3 users,  load average: 2.56, 2.60, 2.46 5a7706d6edb790ff645b32ce2ae9f2237153d9

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -10,8 +10,25 @@ static const char *keyname[256] __attribute__((used)) = {
 //函数声明
 extern void getScreen(int* p_width, int* p_height);
 
-
 size_t events_read(void *buf, size_t len) {
+  char buffer[40];
+  int key = _read_key();
+  int down = 0;
+  if(key & 0x8000) {
+      key ^= 0x8000;
+      down = 1;
+  }
+  if(key != _KEY_NONE) {
+     sprintf(buffer, "%s %s\n", down ? "kd": "ku", keyname[key]);
+  }
+  else {
+      sprintf(buffer,"t %d\n", _uptime());
+  }
+  if(strlen(buffer) <= len) { 
+    strncpy((char*)buf, buffer,strlen(buffer));
+	  return strlen(buffer);
+  } 
+  Log("strlen(event)>len, return 0");
   return 0;
 }
 
