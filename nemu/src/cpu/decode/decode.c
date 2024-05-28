7977e3ceff64e20b1c8c046924d352b6a2f2fb11
tracer-ics2017 <tracer@njuics.org>
2024-05-28 19:19:50 +0800

    >  run 2110508 ybx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:19:50 up 1 day,  2:36,  3 users,  load average: 2.26, 2.15, 2.16 2d52d872e1c2ae6d7339d018d1e6aadfefdb14f

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -38,10 +38,12 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-  op->simm=instr_fetch(eip,op->width);
-  if(op->width==1){
-   op->simm=(int8_t)op->simm;
+  //TODO();
+  op -> simm = instr_fetch(eip, op -> width);
+  if(op -> width == 1) {
+    op -> simm = (int8_t)op -> simm;
   }
+
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
@@ -246,7 +248,7 @@ make_DHelper(gp2_Ib2E) {
 
 /* Ev <- GvIb
  * use for shld/shrd */
-make_DHelper(I_G2E) {
+make_DHelper(Ib_G2E) {
   decode_op_rm(eip, id_dest, true, id_src2, true);
   id_src->width = 1;
   decode_op_I(eip, id_src, true);
@@ -295,12 +297,6 @@ make_DHelper(out_a2I) {
   decode_op_I(eip, id_dest, true);
 }
 
-// make_DHelper(Ib_G2E) {
-//   decode_op_rm(eip, id_dest, true, id_src2, true);
-//   id_src->width = 1;
-//   decode_op_I(eip, id_src, true);
-// }
-
 make_DHelper(out_a2dx) {
   decode_op_a(eip, id_src, true);
 
@@ -310,6 +306,10 @@ make_DHelper(out_a2dx) {
 #ifdef DEBUG
   sprintf(id_dest->str, "(%%dx)");
 #endif
+}
+
+make_DHelper(lidt_a) {
+  decode_op_a(eip, id_dest, true);
 }
 
 void operand_write(Operand *op, rtlreg_t* src) {
