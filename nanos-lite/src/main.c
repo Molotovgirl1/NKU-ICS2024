33691ae9300ba499467e2e899ec6cb971a1fa175
tracer-ics2017 <tracer@njuics.org>
2024-05-28 19:48:48 +0800

    >  run 2110508 ybx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:48:48 up 1 day,  3:05,  3 users,  load average: 2.21, 2.15, 2.11 91b1456a102cc35a918ff18fcbc460b4835b774e

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -31,7 +31,7 @@ int main() {
 
   init_fs();
 
-  // uint32_t entry = loader(NULL, "/bin/dummy");
+  // uint32_t entry = loader(NULL, "/bin/pal");
   load_prog("/bin/dummy");
   // ((void (*)(void))entry)();
   _trap();
