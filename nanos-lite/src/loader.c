7fbff628843a3031fdbc383c7ca162f17ac10062
tracer-ics2017 <tracer@njuics.org>
2024-05-28 19:37:22 +0800

    >  run 2110508 ybx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:37:22 up 1 day,  2:53,  3 users,  load average: 2.18, 2.17, 2.13 4fb6a8b1b5491ff82ef874b02ccb2b48b8d3165e

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,23 +3,31 @@
 #define DEFAULT_ENTRY ((void *)0x8048000)
 
 extern int fs_open(const char *,int,int);
+extern void fs_close(int);
 extern size_t fs_filesz(int);
 extern void fs_read(int ,void *,size_t);
 extern void * new_page(void );
-uintptr_t loader(_Protect *as, const char *filename) {
 
-      Log("%s",filename);
-      int fd=fs_open(filename,0,0);
-     size_t size=fs_filesz(fd);
-//      fs_read(fd,DEFAULT_ENTRY,size);
- 	void *va,*pa;
-	va=DEFAULT_ENTRY;
-	for(;va<size+DEFAULT_ENTRY;va+=PGSIZE){
-	pa=new_page();
-    Log("%x:%x",(uint32_t)pa,(uint32_t)va);
-	_map(as,va,pa);
-	fs_read(fd,pa,PGSIZE);
-	}
-		
-    return (uintptr_t)DEFAULT_ENTRY;
+uintptr_t loader(_Protect *as, const char *filename) {
+  // TODO();
+  // ramdisk_read(DEFAULT_ENTRY, 0, RAMDISK_SIZE);
+  int fd = fs_open(filename, 0, 0);
+  Log("filename=%s,fd=%d",filename,fd);
+  // fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  int size = fs_filesz(fd);
+  int ppnum = size / PGSIZE;
+  if(size % PGSIZE != 0) {
+     ppnum++;
+   }
+  void *pa = NULL;
+  void *va = DEFAULT_ENTRY;
+  for(int i = 0; i < ppnum; i++) {
+  pa = new_page();
+  _map(as, va, pa);
+  fs_read(fd, pa, PGSIZE);
+  va += PGSIZE;
+  }
+ 
+  fs_close(fd);
+  return (uintptr_t)DEFAULT_ENTRY;
 }
