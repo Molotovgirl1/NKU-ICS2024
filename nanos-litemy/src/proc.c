bef20ceb0f2b43b5b452e6965d5447f5424704be
tracer-ics2017 <tracer@njuics.org>
2024-05-29 16:10:32 +0800

    >  run 2013750 ybx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:10:32 up 1 day,  7:50,  3 users,  load average: 2.52, 2.63, 2.59 940add3bc3ddd64faa7b6a9ff388c3999c5b8c65

---
+++ nanos-litemy/src/proc.c
@@ -0,0 +1,37 @@
+#include "proc.h"
+
+#define MAX_NR_PROC 4
+
+static PCB pcb[MAX_NR_PROC];
+static int nr_proc = 0;
+PCB *current = NULL;
+
+uintptr_t loader(_Protect *as, const char *filename);
+
+void load_prog(const char *filename) {
+  int i = nr_proc ++;
+  _protect(&pcb[i].as);
+
+  uintptr_t entry = loader(&pcb[i].as, filename);
+
+  // TODO: remove the following three lines after you have implemented _umake()
+  //_switch(&pcb[i].as);
+  //current = &pcb[i];
+  //((void (*)(void))entry)();
+
+  _Area stack;
+  stack.start = pcb[i].stack;
+  stack.end = stack.start + sizeof(pcb[i].stack);
+
+  pcb[i].tf = _umake(&pcb[i].as, stack, stack, (void *)entry, NULL, NULL);
+}
+
+_RegSet* schedule(_RegSet *prev) {
+     if(current != NULL) {
+ current-> tf = prev;
+ }
+ current = (current == &pcb[0]? &pcb[1] : &pcb[0]);
+ Log("ptr = 0x%x\n", (uint32_t)current-> as.ptr);
+ _switch(&current-> as);
+ return current-> tf;
+}
