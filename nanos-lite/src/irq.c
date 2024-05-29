b7140447df92f81dcc37db6f565c977a424b881e
tracer-ics2017 <tracer@njuics.org>
2024-05-29 15:17:39 +0800

    >  run 2110508 ybx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:17:39 up 1 day,  6:57,  3 users,  load average: 3.11, 2.65, 2.43 6bfb3572b679471830cf3c55ae2c348af6298898

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,12 +2,14 @@
 
 extern _RegSet* do_syscall(_RegSet *r);
 extern _RegSet* schedule(_RegSet*);
-static _RegSet* do_event(_Event e, _RegSet* r) {
+static _RegSet* do_event(_Event e, _RegSet* r)
+{
   switch (e.event) {
     case _EVENT_SYSCALL:
 //      return do_syscall(r);
 	 do_syscall(r);
- 	 return schedule(r);
+	 break;
+ 	 //return schedule(r);
     case _EVENT_TRAP:
 	 printf("event:self-trapped\n");
 	 return schedule(r);
